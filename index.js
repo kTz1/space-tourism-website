eea413c5abf45657f7bff92377797bd1c5975e68
kTz1 <catalin_simon90@yahoo.com>
2022-02-24 12:05:20 +0200

    added the functionality for hamburger menu

---
+++ index.js
@@ -0,0 +1,14 @@
+// Hamburger Menu
+const nav = document.querySelector(".primary-nav");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+    const visibility = nav.getAttribute("data-visible");
+    if(visibility === "false") {
+        nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    } else {
+        nav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
+});
